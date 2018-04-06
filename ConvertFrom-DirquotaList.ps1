6b3c347aa4d7de4e04b4b400dc606705f2e4248e
Adam Mnich <adammnich@gmail.com>
2018-04-06 11:33:21 +0200

    Update ConvertFrom-DirquotaList.ps1
    Added example to help

--- ConvertFrom-DirquotaList.ps1
+++ ConvertFrom-DirquotaList.ps1
@@ -2,6 +2,25 @@ function ConvertFrom-DirquotaList {
     <#
 	.SYNOPSIS
 	Convert output from Dirquota quota list to an object
+	
+	.EXAMPLE
+	
+	PS > & Dirquota quota list | ConvertFrom-DirquotaList
+	
+	QuotaPath     : D:\Share\user
+	SharePath     : {\\SERVER\Share\user}
+	LimitGB       : 3,5
+	UsedGB        : 3,4
+	UsedPC        : 97
+	AvailableGB   : 0,1
+	AvailablePC   : 3
+	PeakGB        : 13,75
+	PeakDate      : 2018-01-22 12:44
+	Template      : Share template
+	TemplateMatch : Matches template
+	LimitType     : Soft
+	Status        : Enabled
+	
 	.NOTES
 	Original code from Technet gallery by Ben Wilkinson https://gallery.technet.microsoft.com/scriptcenter/Find-and-Report-on-cc49120e
 #>
